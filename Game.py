226fb35563af37a7597cb03b6789182767ced49a
Grégoire Apostoloff <gregoire.ap@gmail.com>
2023-05-25 23:42:17 +0200

    "Amélioration du code: rejouer, comptage"

--- Game.py
+++ Game.py
@@ -7,25 +7,43 @@ df.head()
 
 #  Application
 # Sélectionnez un joueur au hasard
-joueur = df.sample().iloc[0]
+rejouer = True
+score = 0
 
-# Affiche les informations sélectionnées
-print("Nationalité:", joueur['Nation'])
-print("Âge:", joueur['Age'])
-print("Poste:", joueur['Pos'])
-print("Qui est ce joueur ?\n")
+while rejouer:
+    joueur = df.sample().iloc[0]
 
-nom = input()
+    # Affiche les informations sélectionnées
+    print("Nationalité:", joueur['Nation'])
+    print("Âge:", joueur['Age'])
+    print("Poste:", joueur['Pos'])
+    print("Qui est ce joueur ?\n")
 
-compteur_essais = 1
+    nom = input()
 
-while nom.lower() not in joueur['Player'].lower():
-    if compteur_essais == 3:
-        print("Désolé, vous avez épuisé vos essais. La réponse est", joueur['Player'])
-        break
+    compteur_essais = 1
+    essais_max = 3
 
-    nom = input("Essaie encore : ")
-    compteur_essais += 1
+    while nom.lower() not in joueur['Player'].lower():
+        if compteur_essais == essais_max:
+            print("Désolé, c'est perdu. La réponse est", joueur['Player'])
+            score -= 1  # Décrémente le score en cas d'échec après 3 essais
+            break
 
-if nom.lower() in joueur['Player'].lower():
-    print("Bravo, c'est gagné !")
+        nom = input("Essaie encore : ")
+        compteur_essais += 1
+
+    if nom.lower() == joueur['Player'].lower():
+        print("Bravo, c'est gagné !")
+        score += 1  # Incrémente le score en cas de bonne réponse
+    elif nom.strip() == '':
+        print("Le joueur était", joueur['Player'])
+        score -= 1  # Décrémente le score en cas de réponse vide
+
+    print("Score actuel:", score)
+
+    choix_rejouer = input("Voulez-vous rejouer ? (Oui/Non) ")
+
+    if choix_rejouer.lower() != "oui":
+        print("Merci à bientôt !")
+        rejouer = False
